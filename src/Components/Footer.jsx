a731b93a4fce8af7b0e7706989620df257834ac2
Edzy <edwinsiby16@gmail.com>
2022-09-07 18:03:21 +0530

    Project Completed

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const currentyear = new Date().getFullYear();
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright @ {currentyear}</p>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/Components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1>This is the React Bootcamp project</h1>
+      <p>Submitted by Edwin Siby</p>
+    </div>
+  );
+}
+
+export default Notes;
